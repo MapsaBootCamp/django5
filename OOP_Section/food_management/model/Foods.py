ee51a38981db544f0adfc31c4f70961361791eea
Teghfo <teghfo@gmail.com>
2021-07-08 15:56:02 +0430

    update Food model

--- OOP_Section/food_management/model/Foods.py
+++ OOP_Section/food_management/model/Foods.py
@@ -1,9 +1,28 @@
 class Category:
+    """
+    نوع غذا براساس این کلاس مشخص میشود. کتگوری با صرفا یک ويژگی نام مشخص میگردد
+    """
+
     def __init__(self, name):
         self.name = name
 
 
+class SubCategory(Category):
+    def __init__(self, category: Category, name: str):
+        super().__init__(name)
+        self.category = category
+
+
 class Food:
-    pass
+    """
+    کلاس بیس همه ی غذاهای موجود در سیستم
+    status : true ---> available, false ---> finish
+    """
 
+    def __init__(self, sub_category: SubCategory, status: bool, ):
+        self.id =
+        self.sub_category = sub_category
+        self.price = None
 
+    def set_price(self, price: int):
+        self.price = price

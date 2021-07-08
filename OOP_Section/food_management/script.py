b8feb35fbb34d348991b70a81bf0f008e0c375bd
Teghfo <teghfo@gmail.com>
2021-07-08 15:55:11 +0430

    create user file

---
+++ OOP_Section/food_management/script.py
@@ -0,0 +1,20 @@
+import csv
+from pathlib import Path
+
+BASE_PATH = Path(__file__).resolve().parent
+
+
+def create_user_file():
+    try:
+        with open(BASE_PATH / "user.csv") as f:
+            pass
+    except IOError:
+        fieldnames = ['id', "username", "password",
+                      "age", "address", "email", "phone_number"]
+        with open(BASE_PATH / "user.csv", 'a', newline='') as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writeheader()
+
+
+if __name__ == "__main__":
+    create_user_file()

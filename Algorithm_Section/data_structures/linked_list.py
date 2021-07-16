e66fbd06c8627d8cc0d86c84616591393d5abd99
Teghfo <teghfo@gmail.com>
2021-07-16 13:19:15 +0430

    add some functionality to linkedlist

--- Algorithm_Section/data_structures/linked_list.py
+++ Algorithm_Section/data_structures/linked_list.py
@@ -32,6 +32,60 @@ class LinkedList:
             self.root = node
             node.node_busy()
 
+    def append(self, node: LinkedListNode):
+        assert node.free == True, "node must be free. ke nist!"  # tak par bash!
+        if not self.root:
+            self.root = node
+            node.node_busy()
+        else:
+            temp = self.root
+            # while temp.next:
+            #     temp = temp.next
+            # temp.next = node
+            # node.node_busy()
+            while temp:
+                if not temp.next:
+                    temp.next = node
+                    node.node_busy()
+                    break
+                else:
+                    temp = temp.next
+
+    def __len__(self):
+        count = 0
+        if not self.root:
+            return 0
+        else:
+            temp = self.root
+            while temp:
+                temp = temp.next
+                count += 1
+            return count
+
+    def pop(self, index=-1):
+        pass
+
+    def insert(self, node: LinkedListNode, index: int):
+        assert node.free == True, "node must be free. ke nist!"  # tak par bash!
+        if not self.root:
+            self.root = node
+            node.node_busy()
+        elif index >= len(self):
+            self.append(node)
+        elif index == 0:
+            self.add_begin(node)
+        else:
+            count = 0
+            temp = self.root
+            while temp:
+                if count == (index-1):
+                    right_temp = temp.next
+                    temp.next = node
+                    node.next = right_temp
+                    break
+                count += 1
+                temp = temp.next
+
     def __str__(self):
         if not self.root:
             return "(]"
@@ -49,8 +103,16 @@ l2 = LinkedList()
 node1 = LinkedListNode("ashkan")
 node2 = LinkedListNode("asghar")
 node3 = LinkedListNode(25)
+node4 = LinkedListNode(8954)
 l1.add_begin(node1)
 l1.add_begin(node2)
-l1.add_begin(node3)
+l1.append(node3)
+l1.insert(node4, 1)
 print(l1)
 print(l2)
+print(len(l2))
+
+
+# a = [2]
+# a.insert(0, "ashkan")
+# print(a)

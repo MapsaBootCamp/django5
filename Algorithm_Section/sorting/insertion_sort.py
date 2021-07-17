af2341809efe87a282b2df4ea973fae519a5bffa
Teghfo <teghfo@gmail.com>
2021-07-17 12:34:19 +0430

    bug fix insertion sort

--- Algorithm_Section/sorting/insertion_sort.py
+++ Algorithm_Section/sorting/insertion_sort.py
@@ -1,22 +1,19 @@
 def insertion_sort(arr: list) -> list:
     for i in range(1, len(arr)):
         if arr[i-1] < arr[i]:
-            print("hellp")
             continue
         else:
             for j in range(i-1, -1, -1):
-                print("bye: ", j)
-                print("i: ", i)
+                flag = True
                 if arr[j] < arr[i]:
-                    print("salam")
                     temp = arr.pop(i)
-                    arr.insert(j, temp)
+                    arr.insert(j+1, temp)
+                    flag = False
                     break
-            temp = arr.pop(i)
-            arr.insert(0, temp)
+            if flag:
+                temp = arr.pop(i)
+                arr.insert(0, temp)
     return arr
 
-
-# print(list(range(0, -1, -1)))
 
 print(insertion_sort([10, 2, 3, 15, -1, 0]))

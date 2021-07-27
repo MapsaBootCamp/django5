f5ab120a8584b4046fa0f3523f4a1b4c6f57df07
Teghfo <teghfo@gmail.com>
2021-07-27 20:31:46 +0430

    show articles views

--- hello_django/app/urls.py
+++ hello_django/app/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from .views import list_authors
+from .views import list_authors, show_articles
 
 urlpatterns = [
     path('authors/', list_authors),
+    path('articles/', show_articles),
+
 ]
--- hello_django/app/views.py
+++ hello_django/app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 
-from .models import Author
+from .models import Author, Article
 
 
 def list_authors(request):
@@ -17,5 +17,19 @@ def list_authors(request):
         )
     return JsonResponse(authors_list, safe=False)
 
-def show_article(request):
-    pass
+def show_articles(request):
+    articles_list = list(Article.objects.filter(status=True).values("title", "body", "author__name","created_time"))
+    # articles_qs = Article.objects.filter(status=True)
+    # articles_list = []
+    # for article in articles_qs:
+    #     articles_list.append(
+    #         {
+    #             "title": article.title,
+    #             "body": article.body,
+    #             "created_time": article.created_time,
+    #             "author": article.author.name
+    #         }
+    #     )
+
+    return JsonResponse(articles_list, safe=False)
+

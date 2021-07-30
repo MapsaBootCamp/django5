35d1dc1f235f4910fc5bf04f060b349dca8b3c20
Teghfo <teghfo@gmail.com>
2021-07-30 14:26:23 +0430

    add postgresql to project

--- hello_django/news_agency/settings.py
+++ hello_django/news_agency/settings.py
@@ -79,10 +79,21 @@ WSGI_APPLICATION = 'news_agency.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'rishogheichi',
+        'USER': 'admin',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 

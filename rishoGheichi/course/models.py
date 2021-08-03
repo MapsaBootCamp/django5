e322aa8622d3c13c80b1bdc807171efe0ea7dfc7
Teghfo <teghfo@gmail.com>
2021-08-03 20:01:56 +0430

    create course model

--- rishoGheichi/course/models.py
+++ rishoGheichi/course/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class CourseCategory(models.Model):
+    name = models.CharField(max_length=255)
+    category = models.ForeignKey("self", null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+
+
+class Course(models.Model):
+    pass
--- rishoGheichi/users/models.py
+++ rishoGheichi/users/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class Karjoo(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to="karjoo")

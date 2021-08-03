4999748bb470ad67f4afbe8983fecc1239f18dc0
Teghfo <teghfo@gmail.com>
2021-08-03 19:16:27 +0430

    start all apps:smile:

--- rishoGheichi/rishoGheichi/settings.py
+++ rishoGheichi/rishoGheichi/settings.py
@@ -4,18 +4,9 @@ from dotenv import load_dotenv, find_dotenv
 
 env_file = Path(find_dotenv(usecwd=True))
 load_dotenv(verbose=True, dotenv_path=env_file)
-
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY', 'abc')
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
 ALLOWED_HOSTS = []
 
 # Application definition

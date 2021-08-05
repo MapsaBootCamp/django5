5d8a19ebce574ebfa95e1e6e042dbd9a341cf526
Teghfo <teghfo@gmail.com>
2021-08-05 19:43:45 +0430

    custom user

--- rishoGheichi/rishoGheichi/settings.py
+++ rishoGheichi/rishoGheichi/settings.py
@@ -18,7 +18,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'users.apps.UsersConfig',
+
+    "users",
 ]
 
 MIDDLEWARE = [
@@ -32,6 +33,7 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'rishoGheichi.urls'
+AUTH_USER_MODEL = 'users.User'
 
 TEMPLATES = [
     {
--- rishoGheichi/users/models.py
+++ rishoGheichi/users/models.py
@@ -1,8 +1,54 @@
+from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.models import AbstractUser, User
 from django.db import models
-from django.contrib.auth import get_user_model
 from utils.models_utils import model_image_directory_path
+from django.utils.translation import gettext as _
 
-User = get_user_model()
+
+class CustomUserManager(BaseUserManager):
+    """
+    Custom user model manager where email is the unique identifiers
+    for authentication instead of usernames.
+    """
+
+    def create_user(self, email, password, phone="", **extra_fields):
+
+        if not email:
+            raise ValueError('The Email must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, phone=phone, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password, phone="",  **extra_fields):
+        """
+        Create and save a SuperUser with the given email and password.
+        """
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+        return self.create_user(email, password,phone, **extra_fields)
+
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=250)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+    objects = CustomUserManager()
+
+    class Meta:
+        db_table = 'auth_user'
+        verbose_name = _('user')
+        verbose_name_plural = _('users')
 
 
 class BaseKarbaran(models.Model):
@@ -16,9 +62,7 @@ class BaseKarbaran(models.Model):
 class Karjoo(BaseKarbaran):
     pass
 
-    @property
-    def karjoo_name(self):
-        return
+
 
 
 class KarFarma(BaseKarbaran):

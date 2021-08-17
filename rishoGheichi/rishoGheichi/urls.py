0b4d7907a5e6bff265e9b192d6a897da55369383
Teghfo <teghfo@gmail.com>
2021-08-17 19:16:42 +0430

    start templates

--- rishoGheichi/rishoGheichi/urls.py
+++ rishoGheichi/rishoGheichi/urls.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
 from django.urls import path
+from django.shortcuts import render
+
+
+def index(request):
+    context = {"name": "ashkan", "age": 19}
+    return render(request, 'index.html', context)
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index, name='home_page')
 ]

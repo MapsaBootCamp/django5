081d8578fd53c4ab8c02e247beb23db5ffe8b226
Teghfo <teghfo@gmail.com>
2021-08-31 14:28:16 +0430

    add login view

--- rishoGheichi/users/views.py
+++ rishoGheichi/users/views.py
@@ -6,7 +6,7 @@ from django.contrib.auth import authenticate, login as log_in, logout as log_out
 def login(request):
     if request.method == "GET":
         return render(request, "login.html", {})
-    elif request.method =="POST":
+    elif request.method == "POST":
         email = request.POST.get("email", None)
         if not email:
             return render(request, "404.html", {})
@@ -24,6 +24,7 @@ def login(request):
 
 def logout(request):
     pass
+
 
 def register(request):
     pass

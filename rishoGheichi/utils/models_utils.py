715f3e11a8ad378158747562875ebece30c3e267
Teghfo <teghfo@gmail.com>
2021-09-10 23:03:47 +0430

    image upload name

--- rishoGheichi/utils/models_utils.py
+++ rishoGheichi/utils/models_utils.py
@@ -1,4 +1,6 @@
 def model_image_directory_path(instance, filename):
     # file will be uploaded to MEDIA_ROOT / user_<id>/<filename>
-    return f'{instance.__class__.__name__}/{instance.id}_{instance}'
+    file_type = filename.split('.')[-1]
+    username = instance.user.email.split("@")[0]
+    return f'{instance.__class__.__name__}/{instance.id}_{username}.{file_type}'
 

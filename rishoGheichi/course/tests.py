73fad824881df04f56a7758821302a2ca537af1f
teghfo <teghfo@gmail.com>
2021-10-01 10:53:13 +0330

    unit test chist?

--- rishoGheichi/course/tests.py
+++ rishoGheichi/course/tests.py
@@ -1,3 +1,41 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+# class CourseCommnetTest(TestCase):
+#     pass
+
+
+class Calculator:
+    def sum(self, a, b):
+        return a+b+1
+
+    def multiply(self, a, b):
+        return a*b
+
+
+class CalculatorTest(TestCase):
+
+    def setUp(self):
+        print("in setup")
+
+    @classmethod
+    def setUpClass(cls):
+        cls.cal = Calculator()
+        print("in class setUp")
+
+    def test_sum(self):
+        a, b = 5, 8
+        c = self.cal.sum(a, b)
+        self.assertEqual(c, 13)
+
+    def test_multiply(self):
+        a, b = 5, 8
+        c = self.cal.multiply(a, b)
+        self.assertEqual(c, 40)
+
+    def tearDown(self):
+        print("in tearDoen")
+
+    @classmethod
+    def tearDownClass(cls):
+        print("in class tearDown")
